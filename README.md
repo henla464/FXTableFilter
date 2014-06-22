8d006abebd1b59f83bc149b24f14d46b0e9850b6
Henrik Larsson <laselase@gmail.com>
2014-06-22 20:48:56 +0200

    new version, new examples

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ FXTableFilter (Fast eXtensible TableFilter)
 
 FX TableFilter is a JQuery plugin that adds filter capabilities to an HTML table. This plugin is a rewrite of the PicNet Table Filter, it is intended to be Faster and more eXtensible. It works on larger tables and custom filters can be added.
 
-Demos can be found here: http://henla464.github.io/FXTableFilter/
+Demos can be found here: [http://henla464.github.io/FXTableFilter/](http://henla464.github.io/FXTableFilter/)
 
 The rewrite has focused on:
 * Performance. To make it work better on larger tables, with more columns and rows.
@@ -16,9 +16,20 @@ Main disadvantages compared to the PicNet Table Filter
 
 
 Version History
-===============
-Version 3
+=============
+Version 5
+- Changed to only use a single tableFilter() function (best practice). 
+  * tableFilterReload() -> is now called like tableFilter('reload')
+  * tableFilterClear -> tableFilter('clearAllFilters')
+  * tableFilterApplyFilter -> tableFilter('applyFilter')
+
+Version 4
 - Added a tableFilterReload() function to reload the internal cache with new/changed/updated rows
+
+Version 3
+- Changes to configuration, added support for multiple header rows
+- Added possibility to exclude some columns, so that the values are not stored internally
+- Added several new demo pages
 
 Version 2
 - First version uploaded to github
--- TableFilter.js
+++ TableFilter.js
@@ -2,6 +2,94 @@ window.picnet = window.picnet || {};
 window.picnet.ui = window.picnet.ui || {};
 window.picnet.ui.filter = window.picnet.ui.filter || {};
 
+(function ($) {
+    $.fn.tableFilter = function(method) {
+        // plugin's default options
+        var defaults = {
+            thead:               null,
+            columnFilters:       [],
+            additionalFilters:   [],
+            clearFilterControls: [],
+            filterDelay:         200,
+            enableCookies:       true,
+            filteringRows:       function() {},
+            filteredRows:        function() {},
+            shouldFilterRow:     null,
+            useAsyncInit:        false
+        };
+        var settings = {};
+
+        var methods = {
+            init: function (options) {
+                settings = $.extend({}, defaults, options);
+                // iterate through all the DOM elements we are attaching the plugin to
+                return this.each(function () {
+                    var $self = $(this); // reference the jQuery version of the current DOM element
+                    methods.setup.apply(this, Array.prototype.slice.call(arguments, 1));
+                });
+            },
+            // setup each table
+            setup: function() {
+                var jqSelf = $(this);
+                var tableFilterObj = jqSelf.data('tableFilter');
+                //if (tableFilterObj === undefined || tableFilterObj === null) {
+                    var tableFilterObj = new picnet.ui.filter.TableFilter(this, settings);
+                    jqSelf.data('tableFilter', tableFilterObj);  
+                //}
+                return this;
+            },
+
+            applyFilter: function () {
+                // iterate through all the DOM elements we are attaching the plugin to
+                return this.each(function () {
+                    var tableFilterObj = $(this).data('tableFilter');
+                    if (tableFilterObj === undefined || tableFilterObj === null) {
+                        $.error('TableFilter not initialized!');
+                    } else {
+                        tableFilterObj.applyFilter();
+                    }
+                });
+            },
+
+            clearAllFilters: function () {
+                // iterate through all the DOM elements we are attaching the plugin to
+                return this.each(function () {
+                    var tableFilterObj = $(this).data('tableFilter');
+                    if (tableFilterObj === undefined || tableFilterObj === null) {
+                        $.error('TableFilter not initialized!');
+                    } else {
+                        tableFilterObj.clearAllFilters();
+                    }
+                });
+            },
+
+            reload: function () {
+                // iterate through all the DOM elements we are attaching the plugin to
+                return this.each(function () {
+                    var tableFilterObj = $(this).data('tableFilter');
+                    if (tableFilterObj === undefined || tableFilterObj === null) {
+                        $.error('TableFilter not initialized!');
+                    } else {
+                        tableFilterObj.reload();
+                    }
+                });
+            }
+        };
+
+        // if a method as the given argument exists (arguments variable is always available in a function)
+        if (methods[method]) {
+           return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            // call the initialization method
+            return methods.init.apply(this, arguments);
+        } else {
+            // trigger an error
+            $.error('Method "' +  method + '" does not exist in tableFilter plugin!');
+        }
+    };
+})(jQuery);
+
+
 
 //////////////
 picnet.ui.filter.TableFilterRow = function(tr, columnNames, onlyTexts, includeDatas) {
@@ -299,7 +387,12 @@ picnet.ui.filter.SearchEngine.EPrecedence = {
 /* INIT START */
 picnet.ui.filter.TableFilter = function(table, options) {    
     // DOM Objects
-    this.thead = options.thead ? options.thead : table.getElementsByTagName('thead')[0];    
+    if (options.thead && options.thead.jquery) {
+        this.thead = options.thead[0];
+    } else {
+        this.thead = table.getElementsByTagName('thead')[0];
+    }
+
     this.tbody = table.getElementsByTagName('tbody')[0];
     
     this.tableFilterRows = []; //  !TableFilterRow[]
@@ -954,4 +1047,194 @@ picnet.ui.filter.TableFilter.prototype.getFilterStateForFilter = function(filter
     var state = new picnet.ui.filter.FilterState(filterCtrl.attr('id'), value, columnIndex, type);
         
     return state;
+};
+
+
+
+
+//
+
+
+
+/**
+ * jQuery.timers - Timer abstractions for jQuery
+ * Written by Blair Mitchelmore (blair DOT mitchelmore AT gmail DOT com)
+ * Licensed under the WTFPL (http://sam.zoy.org/wtfpl/).
+ * Date: 2009/10/16
+ *
+ * @author Blair Mitchelmore
+ * @version 1.2
+ *
+ **/
+
+jQuery.fn.extend({
+	everyTime: function(interval, label, fn, times) {
+		return this.each(function() {
+			jQuery.timer.add(this, interval, label, fn, times);
+		});
+	},
+	oneTime: function(interval, label, fn) {
+		return this.each(function() {
+			jQuery.timer.add(this, interval, label, fn, 1);
+		});
+	},
+	stopTime: function(label, fn) {
+		return this.each(function() {
+			jQuery.timer.remove(this, label, fn);
+		});
+	}
+});
+
+jQuery.extend({
+	timer: {
+		global: [],
+		guid: 1,
+		dataKey: "jQuery.timer",
+		regex: /^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/,
+		powers: {
+			// Yeah this is major overkill...
+			'ms': 1,
+			'cs': 10,
+			'ds': 100,
+			's': 1000,
+			'das': 10000,
+			'hs': 100000,
+			'ks': 1000000
+		},
+		timeParse: function(value) {
+			if (value == undefined || value == null)
+				return null;
+			var result = this.regex.exec(jQuery.trim(value.toString()));
+			if (result[2]) {
+				var num = parseFloat(result[1]);
+				var mult = this.powers[result[2]] || 1;
+				return num * mult;
+			} else {
+				return value;
+			}
+		},
+		add: function(element, interval, label, fn, times) {
+			var counter = 0;
+			
+			if (jQuery.isFunction(label)) {
+				if (!times) 
+					times = fn;
+				fn = label;
+				label = interval;
+			}
+			
+			interval = jQuery.timer.timeParse(interval);
+
+			if (typeof interval != 'number' || isNaN(interval) || interval < 0)
+				return;
+
+			if (typeof times != 'number' || isNaN(times) || times < 0) 
+				times = 0;
+			
+			times = times || 0;
+			
+			var timers = jQuery.data(element, this.dataKey) || jQuery.data(element, this.dataKey, {});
+			
+			if (!timers[label])
+				timers[label] = {};
+			
+			fn.timerID = fn.timerID || this.guid++;
+			
+			var handler = function() {
+				if ((++counter > times && times !== 0) || fn.call(element, counter) === false)
+					jQuery.timer.remove(element, label, fn);
+			};
+			
+			handler.timerID = fn.timerID;
+			
+			if (!timers[label][fn.timerID])
+				timers[label][fn.timerID] = window.setInterval(handler,interval);
+			
+			this.global.push( element );
+			
+		},
+		remove: function(element, label, fn) {
+			var timers = jQuery.data(element, this.dataKey), ret;
+			
+			if ( timers ) {
+				
+				if (!label) {
+					for ( label in timers )
+						this.remove(element, label, fn);
+				} else if ( timers[label] ) {
+					if ( fn ) {
+						if ( fn.timerID ) {
+							window.clearInterval(timers[label][fn.timerID]);
+							delete timers[label][fn.timerID];
+						}
+					} else {
+						for ( var fn in timers[label] ) {
+							window.clearInterval(timers[label][fn]);
+							delete timers[label][fn];
+						}
+					}
+					
+					for ( ret in timers[label] ) break;
+					if ( !ret ) {
+						ret = null;
+						delete timers[label];
+					}
+				}
+				
+				for ( ret in timers ) break;
+				if ( !ret ) 
+					jQuery.removeData(element, this.dataKey);
+			}
+		}
+	}
+});
+
+jQuery(window).bind("unload", function() {
+	jQuery.each(jQuery.timer.global, function(index, item) {
+		jQuery.timer.remove(item);
+	});
+});
+
+
+/**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+ 
+jQuery.cookie = function (key, value, options) {
+
+    // key and at least value given, set cookie...
+    if (arguments.length > 1 && String(value) !== "[object Object]") {
+        options = jQuery.extend({}, options);
+
+        if (value === null || value === undefined) {
+            options.expires = -1;
+        }
+
+        if (typeof options.expires === 'number') {
+            var days = options.expires, t = options.expires = new Date();
+            t.setDate(t.getDate() + days);
+        }
+
+        value = String(value);
+
+        return (document.cookie = [
+            encodeURIComponent(key), '=',
+            options.raw ? value : encodeURIComponent(value),
+            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
+            options.secure ? '; secure' : ''
+        ].join(''));
+    }
+
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
 };
